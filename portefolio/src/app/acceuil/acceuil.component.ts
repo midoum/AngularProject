285e97b0a5cba7aa5e00edf37ced3b1d0ebfcbca
Ahmed Meskine <40129162+midoum@users.noreply.github.com>
2020-10-19 14:57:54 +0100

    e

--- portefolio/src/app/acceuil/acceuil.component.ts
+++ portefolio/src/app/acceuil/acceuil.component.ts
@@ -9,9 +9,9 @@ import {carte}from './carte';
 export class AcceuilComponent implements OnInit {
 cartes:carte[]=[
   {title:'Pfe',description:'Project robot Arduino contrôler par une application java',link:'github.com/midoum/Projet-PFE.git',image:'1.jpg'},
-  {title:'DesktopOrganizer',description:'application java qui permet de créer des pages des application à lancer directement',link:'github.com/midoum/DesktopOrganize',image:'2.PNG'},
-  {title:'PorteFolio',description:'SiteWeb pour mon CV avec angulair',link:'github.com/midoum/AngularProject',image:'3.PNG'},
-  {title:'E-shop',description:'SiteWeb pour e-shop avec angulair',link:'midoum.github.io/Echri/.',image:'shop.PNG'},
+  {title:'DesktopOrganizer',description:'application java qui permet de créer des pages des application à lancer directement',link:'github.com/midoum/DesktopOrganize',image:'2.png'},
+  {title:'PorteFolio',description:'SiteWeb pour mon CV avec angulair',link:'github.com/midoum/AngularProject',image:'3.png'},
+  {title:'E-shop',description:'SiteWeb pour e-shop avec angulair',link:'midoum.github.io/Echri/.',image:'shop.png'},
   
 
 ];
